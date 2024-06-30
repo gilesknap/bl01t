eef7f2fc8ef0c5f689bb3356abcfea94b9855a14
Giles Knap <giles.knap@diamond.ac.uk>
2024-06-30 11:15:55 +0100

    add opi_auto volume
    as a workaround to podman's inability to use sub_path volume
    mounts
    
    see https://github.com/containers/podman/issues/20661

--- include/init.sh
+++ include/init.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-# The init service shell script - initialises the opi volume with any custom
+# The init service shell script - initialises the opi volumes with any custom
 # bob files from this repo and ensures that each IOC has a subfolder in the
-# volume.
+# volumes.
 
 # copy in any opi files from the repo
 cp -r /repo/opi/* /opi;
 
-# make sure there is a subfolder of opi volume for each service
+# make sure there is a subfolder of opi volumes for each service
 cd /repo/services
 for i in *; do
   echo adding folder $i to opi volume
   mkdir -p /opi/$i;
+  mkdir -p /opi_auto/$i;
 done
