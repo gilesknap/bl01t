08872bd0693e464a81229179d2f06bc12dde55e5
Giles Knap <giles.knap@diamond.ac.uk>
2024-07-23 10:38:35 +0100

    switch to bind mounts for opis

--- include/init.sh
+++ include/init.sh
@@ -6,11 +6,3 @@
 
 # copy in any opi files from the repo
 cp -r /repo/opi/* /opi;
-
-# make sure there is a subfolder of opi volumes for each service
-cd /repo/services
-for i in *; do
-  echo adding folder $i to opi volume
-  mkdir -p /opi/$i;
-  mkdir -p /opi_auto/$i;
-done
