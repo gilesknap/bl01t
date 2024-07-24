a2c191586f35eea8db577054d1a57df0507ef982
Giles Knap <giles.knap@diamond.ac.uk>
2024-07-24 07:03:12 +0100

    move generated bobs to opi/iocs

--- include/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# The init service shell script - initialises the opi volumes with any custom
-# bob files from this repo and ensures that each IOC has a subfolder in the
-# volumes.
-
-# copy in any opi files from the repo
-cp -r /repo/opi/* /opi;
